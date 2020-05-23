83768967e0d343d3be4a220dcfacaccc8cdbc263
Matteo Bernardini <mat_teobe@yahoo.it>
2020-05-23 15:36:36 +0200

    melody: generation inside a class

--- src/core/melody/melody.js
+++ src/core/melody/melody.js
@@ -1,60 +1,71 @@
 import grades_rules from "./grades.yml"
-import { Grammar } from "./grammar.js";
+import { Grammar } from "./grammar.js"
+import { Stream } from "music21j"
 
-// generate a motif, shortest subdivision
-function get_motif() {
-	// call gen_duration_sequence and gen_grades_sequence
-	// apply key and scale
-	// spit out final notes
-}
+class MelodyGenerator {
+
+	constructor() {
+		this.valence = 1
+		this.arousal = 1
+	}
+
+	// generate a motif, shortest subdivision
+	get_motif() {
+		// call gen_duration_sequence and gen_grades_sequence
+		// apply key and scale
+		// spit out final notes
+	}
 
 
-// generate a theme
-function get_theme() {
+	// generate a theme
+	get_theme() {
 
-}
+	}
 
 
-function gen_duration_sequence(N){
-	// TODO: use Grammar class
-	let ruleset_dur = new Map();
-	// duration sequence start with -1
-	ruleset_dur.set("-1", [
-		["1 ", 0.2],
-		["0.5 ", 0.5],
-		["0.25 ", 0.15],
-		["0.125 ", 0.1],
-		["0 ", 0.05]
-	])
+	gen_duration_sequence(N) {
+		// TODO: use Grammar class
+		let ruleset_dur = new Map();
+		// duration sequence start with -1
+		ruleset_dur.set("-1", [
+			["1 ", 0.2],
+			["0.5 ", 0.5],
+			["0.25 ", 0.15],
+			["0.125 ", 0.1],
+			["0 ", 0.05]
+		])
 
-	function pick_string(rule, n) {
-		// distribute rules on a line and pick
-		let low = 0;
-		for (let chance of rule) {
-			if (n >= low && n < chance[1] + low)
-				return chance[0]
-			low += chance[1]
+		function pick_string(rule, n) {
+			// distribute rules on a line and pick
+			let low = 0;
+			for (let chance of rule) {
+				if (n >= low && n < chance[1] + low)
+					return chance[0]
+				low += chance[1]
+			}
+			// in case probabilities don't add up precisely, return last item
+			return rule[rule.length-1][0]
 		}
-		// in case probabilities don't add up precisely, return last item
-		return rule[rule.length-1][0]
+
+		let out =  ""
+		for (let i = 0; i < N; i++) {
+			let rule = ruleset_dur.get("-1")
+			let s = pick_string(rule, Math.random())
+			out += s
+		}
+		return out
 	}
 
-	let out =  ""
-	for (let i = 0; i < N; i++) {
-		let rule = ruleset_dur.get("-1")
-		let s = pick_string(rule, Math.random())
-		out += s
+	// generate sequence of relative grades
+	gen_grades_sequence(N) {
+		let g = new Grammar(grades_rules)
+		g.set_variant(this.valence)
+		return g.generate_sequence(N)
 	}
-	return out
-}
 
-// generate sequence of relative grades
-function gen_grades_sequence(N) {
-	let g = new Grammar(grades_rules)
-	g.set_variant(1) // TODO: change hardcoded variant
-	return g.generate_sequence(N)
 }
 
 // TEST
-console.log(`grades sequence: ${gen_grades_sequence(100)}`)
-console.log(`duration sequence: ${gen_duration_sequence(100)}`)
+let g = new MelodyGenerator()
+console.log(`grades sequence: ${g.gen_grades_sequence(100)}`)
+console.log(`duration sequence: ${g.gen_duration_sequence(100)}`)
