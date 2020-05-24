fd87a47be1819afeb34051dd4406c9201c4d4794
Matteo Bernardini <mat_teobe@yahoo.it>
2020-05-24 16:14:48 +0200

    melody: tiny linting using getter/setters

--- src/core/melody/melody.js
+++ src/core/melody/melody.js
@@ -5,20 +5,44 @@ import { note, stream, pitch, interval } from "music21j/releases/music21.debug" 
 class MelodyGenerator {
 
 	constructor(valence, arousal) {
-		this.valence = valence // 0 to 1
-		this.arousal = arousal // 0 to 1
+		this.valence = valence
+		this.arousal = arousal
+		this._key = new pitch.Pitch(Math.floor(Math.random() * 12))
 	}
 
-	get_key() {
-		return new pitch.Pitch(Math.floor(Math.random() * 12))
+	get valence() {
+		return this._valence
+	}
+	set valence(v) {
+		v = Number(v)
+		if (!(v >= 0 && v <= 1))
+			throw new TypeError("valence should be a value between 0 and 1")
+		this._valence = v
 	}
 
-	get_octave() {
+	get arousal() {
+		return this._arousal
+	}
+	set arousal(a) {
+		a = Number(a)
+		if (!(a >= 0 && a <= 1))
+			throw new TypeError("arousal should be a value between 0 and 1")
+		this._arousal = a
+	}
+
+	get key() {
+		return this._key
+	}
+	set key(k) {
+		this._key = new pitch.Pitch(k)
+	}
+
+	get octave() {
 		const range = [1,2,3,4,5]
 		return range[Math.round(this.valence * (range.length-1))]
 	}
 
-	get_mode() {
+	get mode() {
 		const modes = [ // sorted from darkest to brightest
 			["P1", "M2", "M3", "P4", "P5", "M6", "M7", "P8"],
 			["P1", "M2", "M3", "P4", "P5", "M6", "M7", "P8"],
@@ -31,33 +55,30 @@ class MelodyGenerator {
 		return modes[Math.round(this.valence * (modes.length-1))]
 	}
 
-	get_note(grade, duration) {
+	create_note(grade, duration) {
 		// TODO: rests
-		const mode = this.get_mode()
 		let n = new note.Note()
-		n.pitch = mode[grade-1].transposePitch(this.get_key())
-		n.octave = this.get_octave()
+		n.pitch = this.mode[grade-1].transposePitch(this.key)
+		n.octave = this.octave
 		n.duration = duration
 		return n
 	}
 
 	// generate a motif, shortest subdivision
-	/*music21j.Stream*/ get_motif() {
+	/*music21j.Stream*/ gen_motif() {
 		let grades = this.gen_grades_sequence(100)
 		let durations = this.gen_duration_sequence(grades.length)
-		let notes = grades.map((grade, idx) => this.get_note(grade, durations[idx]))
+		let notes = grades.map((grade, idx) => this.create_note(grade, durations[idx]))
 		let s = new stream.Stream()
 		for (const note of notes)
 			s.append(note)
 		return s
 	}
 
-
 	// generate a theme
-	get_theme() {
+	gen_theme() {
 
 	}
-
 
 	/*float[]*/ gen_duration_sequence(/*int*/ N) {
 		// TODO: use Grammar class
@@ -103,8 +124,6 @@ class MelodyGenerator {
 
 // TEST
 let g = new MelodyGenerator(1,1)
-console.log(`grades sequence: ${g.gen_grades_sequence(100)}`)
-console.log(`duration sequence: ${g.gen_duration_sequence(100)}`)
-
 global.g = g
+
 global.m21j = import("music21j/releases/music21.debug.js")
