2441778406ecbba44f8993c60d87d2115da04ba2
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-08 23:22:45 +0200

    utils: python has `round` and `abs` built-in

--- back-end/src/utils.py
+++ back-end/src/utils.py
@@ -1,15 +1,14 @@
-import math
 
 def select_range(low, up, ratio):
 	"""select a integer value within `low` and `up` (included) based on `ratio`"""
 	diff = up - low
 	shift = low
-	math.round(ratio * diff + shift)
+	round(ratio * diff + shift)
 
 def round_over(val, values):
 	"""round `val` to the closest value in `values`"""
 	rounded = float("infinity")
 	for step in values:
-		if math.abs(val - rounded) > math.abs(val - step):
+		if abs(val - rounded) > abs(val - step):
 			rounded = step
 	return rounded
