2ddbf9cb81fc7c8fea61501c46ef3a05234c4b8d
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-08 23:17:23 +0200

    utils: some docstrings

--- back-end/src/utils.py
+++ back-end/src/utils.py
@@ -1,11 +1,13 @@
 import math
 
 def select_range(low, up, ratio):
+	"""select a integer value within `low` and `up` (included) based on `ratio`"""
 	diff = up - low
 	shift = low
 	math.round(ratio * diff + shift)
 
 def round_over(val, values):
+	"""round `val` to the closest value in `values`"""
 	rounded = float("infinity")
 	for step in values:
 		if math.abs(val - rounded) > math.abs(val - step):
