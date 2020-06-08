4232cba391faae2f2e0182b7fe153228d83661ee
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-08 23:47:47 +0200

    utils: fix, missing return on select_range

--- back-end/src/utils.py
+++ back-end/src/utils.py
@@ -3,7 +3,7 @@ def select_range(low, up, ratio):
 	"""select a integer value within `low` and `up` (included) based on `ratio`"""
 	diff = up - low
 	shift = low
-	round(ratio * diff + shift)
+	return round(ratio * diff + shift)
 
 def round_over(val, values):
 	"""round `val` to the closest value in `values`"""
