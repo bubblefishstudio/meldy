0be7e981c83b9d81abafee2cf516a6afa302e3c9
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-09 09:42:30 +0200

    ci: added build:prod target for gh-pages

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/main.js',
   },
