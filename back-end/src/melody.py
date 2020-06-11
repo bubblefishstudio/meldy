8de07c93e70181489f3efa70705c99d3c2e7f5e1
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-11 22:55:07 +0200

    melody: fix set tempo

--- back-end/src/melody.py
+++ back-end/src/melody.py
@@ -97,7 +97,7 @@ class MelodyGenerator:
 	def gen_melody(self):
 		"""generates final melody"""
 		mel = m21.stream.Part()
-		mel.tempo = self.tempo
+		mel.append(m21.tempo.MetronomeMark(number = self.tempo))
 		mel.timeSignature = self.time_signature
 
 		mot = self.gen_motif() # TODO: to change
