ab56f83e587573609c12ddfb4f975ce618aeed23
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-11 15:25:21 +0200

    main.js: test request view

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,1 +1,10 @@
-// nothing here for now
+import v1 from "./views/request.js"
+
+window.addEventListener("load", () => {
+	let app = document.getElementById("app")
+	let dom1 = v1()
+	if (app.hasChildNodes())
+		app.replaceChild(dom1, app.firstChild)
+	else
+		app.appendChild(dom1)
+})
