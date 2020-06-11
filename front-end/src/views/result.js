c653395b7160e80d1d5dd756c57366872a020aba
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-11 22:56:08 +0200

    score: added playback :D

--- front-end/src/views/result.js
+++ front-end/src/views/result.js
@@ -1,23 +1,29 @@
 import { OpenSheetMusicDisplay } from "opensheetmusicdisplay"
+import AudioPlayer from "osmd-audio-player"
 
 import BaseView from "./base.js"
 
 export default class extends BaseView {
 
 	setup() {
-		// attach musicXML sheet
+		// attach musicXML sheet and AudioPlayer
 		const osmd = new OpenSheetMusicDisplay(this.view.querySelector("#sheet"))
+		this.player = new AudioPlayer()
 		this.data.text()
 			.then(xml => osmd.load(xml))
 			.then(()  => osmd.render())
+			.then(()  => this.player.loadScore(osmd))
 		// bind buttons
-		this.view.querySelector("#play-btn").addEventListener("click", () => this.playScore())
+		this.view.querySelector("#play-btn").addEventListener("click", (e) => this.playScore(e))
 		this.view.querySelector("#dw-btn").addEventListener("click", () => this.downloadScore())
 		this.view.querySelector("#reset-btn").addEventListener("click", () => this.dropScore())
 	}
 
-	playScore() {
-
+	playScore(e) {
+		if (this.player.state === "STOPPED" || this.player.state === "PAUSED")
+			this.player.play()
+		else
+			this.player.stop()
 	}
 
 	downloadScore() {
@@ -25,6 +31,8 @@ export default class extends BaseView {
 	}
 
 	dropScore() {
+		this.player.stop()
+		delete this.player
 		this.navigator.goto("request")
 	}
 
