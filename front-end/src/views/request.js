e1c39245f244685d268b8c3f7297530c4191ddf9
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-11 17:03:30 +0200

    views: request using navigation api

--- front-end/src/views/request.js
+++ front-end/src/views/request.js
@@ -1,9 +1,11 @@
 import p5 from "p5"
 import moodPickerSketch from "../p5/mood_picker_sketch.js"
 
+let navigator
 let mood_picker
 
-export default function() {
+export default function(nav) {
+	navigator = nav
 	const view = require("dom-element-loader!./request.html").default
 	// attach mood-picker canvas
 	mood_picker = new p5(moodPickerSketch, view.querySelector("#mood-picker"))
@@ -14,10 +16,13 @@ export default function() {
 }
 
 function create_melody(e) {
-	console.log(`awesome! this is the value I read: ${mood_picker.readValue()}`)
+	let [v, a] = mood_picker.readValue()
+	fetch_melody(v, a).then((musicxml) => {
+		navigator.goto("result", musicxml)
+	})
 }
 
-async function fetch_melody() {
+async function fetch_melody(v, a) {
 	let response = await fetch(BACKEND_URL)
 	if (response.ok)
 		return response.body
