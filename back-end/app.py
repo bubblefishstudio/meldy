6e8ac9c5da08c96ac94dd6bae994c04200205b05
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-11 16:59:19 +0200

    backend: added CORS to allow dev/prod deploys

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,14 @@
 from tempfile import NamedTemporaryFile
+
 from flask import Flask, Response
+from flask_cors import cross_origin
+
 from src.melody import MelodyGenerator
 
 app = Flask(__name__)
 
 @app.route("/")
+@cross_origin(["http://localhost:8080", "https://mttbernardini.github.io/nuvola"])
 def make_melody():
 	# TODO: receive valence and arousal from client
 	mg = MelodyGenerator(1,1)
