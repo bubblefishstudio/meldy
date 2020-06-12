05ed5d12b15022aa1c8d695436bc19c82d4929d5
mttbernardini <mttbernardini@users.noreply.github.com>
2020-06-12 21:24:46 +0000

    deploy: 69baa29501ed9d2d7fa2f6cf96270e672f3bbc16

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from tempfile import NamedTemporaryFile
-from flask import Flask, Response
+
+from flask import Flask, Response, request
+from flask_cors import cross_origin
+
 from src.melody import MelodyGenerator
 
 app = Flask(__name__)
 
 @app.route("/")
+@cross_origin(["http://localhost:8080", "https://mttbernardini.github.io/nuvola"])
 def make_melody():
-	# TODO: receive valence and arousal from client
-	mg = MelodyGenerator(1,1)
+	valence = request.args.get("valence", default=1, type=float)
+	arousal = request.args.get("arousal", default=1, type=float)
+	mg = MelodyGenerator(valence, arousal)
 	m = mg.gen_melody()
 	with NamedTemporaryFile() as t:
 		m.write("musicxml", fp=t.name)
 		t.seek(0)
-		return Response(t.read(), mimetype="text/xml")
+		return Response(t.read(), mimetype="application/vnd.recordare.musicxml")
