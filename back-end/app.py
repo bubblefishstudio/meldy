93ff982b49029c7b632a15207bd337a7198239a0
yilin zhu <yilinzhu22@gmail.com>
2020-06-14 14:59:59 +0200

    update MelodyGenerator starting from random valence and arousal value

--- back-end/app.py
+++ back-end/app.py
@@ -1,13 +1,15 @@
 from tempfile import NamedTemporaryFile
 from flask import Flask, Response
 from src.melody import MelodyGenerator
+import random 
 
 app = Flask(__name__)
 
 @app.route("/")
 def make_melody():
 	# TODO: receive valence and arousal from client
-	mg = MelodyGenerator(1,1)
+	# temp: valence and arousal are generated from random numbers
+	mg = MelodyGenerator(random.uniform(0,1),random.uniform(0,1))
 	m = mg.gen_melody()
 	with NamedTemporaryFile() as t:
 		m.write("musicxml", fp=t.name)
