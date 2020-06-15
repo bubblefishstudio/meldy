c051883f3a98529fef34b29c9b48431e15904b35
Matteo Bernardini <mat_teobe@yahoo.it>
2020-06-15 18:10:27 +0200

    view: some icons

--- front-end/src/views/request.js
+++ front-end/src/views/request.js
@@ -19,8 +19,8 @@ export default class extends BaseView {
 		// fetch
 		let [v, a] = this.mood_picker.readValue()
 		this.fetch_melody(v, a).then((response) => {
-			this.mood_picker.remove()
 			this.navigator.goto("result", response)
+			this.mood_picker.remove()
 		}).catch(e => {
 			// update ui
 			this.set_loading(false)
